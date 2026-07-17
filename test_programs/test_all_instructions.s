8fe9a541e0aff06dadf71f70fd607b20212502b5
Konstantin Umbereit <konstantin.umbereit@proton.me>
2026-07-17 17:48:20 +0200

    test_programs(test_all_instructions.s): now contains every instrucion, pseudo and directive supported by the assembler

--- test_programs/test_all_instructions.s
+++ test_programs/test_all_instructions.s
@@ -1,80 +1,89 @@
+# test_programs/test_all_instructions.s
+# 
+# Tests all instructions + pseudos + directives supported by the assembler.
+
+.globl _start
 .text
-.global main
-.globl another_label
+.align 4
 
-main:
-    nop
-    mv      x1, x2
-    li      x3, 0x12345678
-    la      x4, data_label
-    j       loop
-    call    subroutine
-    tail    exit
-    ret
+_start:
 
-    addi    x5, x0, 42
-    andi    x6, x1, -1
-    ori     x7, x2, 0xFF
-    xori    x8, x3, 0xAB
-    slli    x9, x4, 3
-    srli    x10, x5, 7
-    srai    x11, x6, 2
-    slti    x12, x7, 100
-    sltiu   x13, x8, 200
+    add     x1, x2, x3
+    sub     x4, x5, x6
+    and     x7, x8, x9
+    or      x10, x11, x12
+    xor     x13, x14, x15
+    sll     x16, x17, x18
+    srl     x19, x20, x21
+    sra     x22, x23, x24
+    slt     x25, x26, x27
+    sltu    x28, x29, x30
 
-    lb      x14,  0(x4)
-    lh      x15,  4(x4)
-    lw      x16,  8(x4)
-    lbu     x17, 12(x4)
-    lhu     x18, 16(x4)
-    jalr    x19, 0(x1)
+    addi    x1, x0, 42
+    andi    x2, x1, 0x0F
+    ori     x3, x2, 0xF0
+    xori    x4, x3, 0xAA
+    slli    x5, x4, 3
+    srli    x6, x5, 2
+    srai    x7, x6, 4
+    slti    x8, x7, 100
+    sltiu   x9, x8, 200
 
-    sb      x20, 20(x4)
-    sh      x21, 22(x4)
-    sw      x22, 24(x4)
+    la      x10, data_start
+    lb      x11, 0(x10)
+    lh      x12, 4(x10)
+    lw      x13, 8(x10)
+    lbu     x14, 12(x10)
+    lhu     x15, 16(x10)
 
-    beq     x1, x2, exit
-    bne     x3, x0, loop
-    blt     x4, x5, exit
-    bge     x6, x7, loop
-    bltu    x8, x9, exit
-    bgeu    x10, x11, loop
+    li      x16, 0xDEADBEEF
+    sb      x16, 20(x10)
+    sh      x16, 24(x10)
+    sw      x16, 28(x10)
 
-    lui     x23, 0x12345
-    auipc   x24, 0x6789A
-    jal     x25, loop
+    beq     x0, x0, br1
+br1:
+    bne     x1, x0, br2
+br2:
+    blt     x0, x1, br3
+br3:
+    bge     x1, x0, br4
+br4:
+    bltu    x0, x1, br5
+br5:
+    bgeu    x1, x0, br6
+br6:
+
+    jal     x17, jal_target
+jal_target:
+    lui     x18, 0x12345
+    auipc   x19, 0x00001
+    jalr    x20, x10, 0
 
     ecall
     ebreak
 
-    add     x26, x27, x28
-    sub     x29, x30, x31
-    and     x1,  x2,  x3
-    or      x4,  x5,  x6
-    xor     x7,  x8,  x9
-    sll     x10, x11, x12
-    srl     x13, x14, x15
-    sra     x16, x17, x18
-    slt     x19, x20, x21
-    sltu    x22, x23, x24
 
-loop:
+    nop
+    mv      x21, x1
+    li      x22, 0xCAFEBABE
+    j       end_label
+    call    dummy_func
+dummy_func:
     ret
+    tail    end_label
 
-subroutine:
-    ret
+end_label:
+    ebreak
 
-exit:
-    ret
-
-another_label:
 
 .data
-data_label:
-    .word   0xdeadbeef, 0xcafebabe
-    .half   0x1234, 0x5678
-    .byte   0xff, 0x00, 0x42
-    .asciz  "Hello, RV32I!"
-    .ascii  "rawbytes"
-    .align  3
+.align 4
+data_start:
     .word   0x11223344
+    .half   0x5566
+    .byte   0x77
+    .ascii  "ABC"
+    .asciz  "Test"
+    .align  4
+    .word   0xDEADBEEF
